462046fcdc623ce158f5e66e944eaab0f5807072
Felix Lai <felix.lai@hotmail.com>
2022-11-11 11:43:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Search from "./components/search/search";
+import CurrentWeather from "./components/current-weather/current-weather";
+import Forecast from "./components/forecast/forecast";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+import "./App.css";
+
 
 function App() {
   return (
