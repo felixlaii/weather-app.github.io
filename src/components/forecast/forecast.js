a8b78898ea1bf4ccf8e34d7ce12389b6c9e16588
Felix Lai <felix.lai@hotmail.com>
2022-11-11 11:22:37 -0500

    Update forecast.js

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -8,3 +8,8 @@ import {
 } from "react-accessible-accordion";
 
 const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+const Forecast = ({ data }) => {
+    const dayInAWeek = new Date().getDay();
+    const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
+}
