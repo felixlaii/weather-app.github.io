c6845bc7223d0f2020c9fc9678657ae49bbc3d54
Felix Lai <felix.lai@hotmail.com>
2022-11-11 11:17:40 -0500

    Update current-weather.js

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -22,7 +22,12 @@ const CurrentWeather = ({ data }) => {
                     <span className="parameter-value">{Math.round(data.main.feels_like)}°C</span>
                 </div>
                 <div className="parameter-row">
-                    
+                    <span className="parameter-label">Wind</span>
+                    <span className="parameter-value">{data.wind.speed}</span>
+                </div>
+                <div className="parameter-row">
+                    <span className="parameter-label">Humidity</span>
+                    <span className="parameter-label">{data.main.humidity}%</span>
                 </div>
             </div>
         </div>
