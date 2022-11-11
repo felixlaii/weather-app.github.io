fccead14ae33ac955365a4dffd96def99c2ba6a0
Felix Lai <felix.lai@hotmail.com>
2022-11-11 11:37:22 -0500

    Update search.js

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,3 +1,14 @@
 import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
 import { geoApiOptions, GEO_API_URL } from "../../api";
+
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    );
+  };
+};
