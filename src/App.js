ad785d137a7925518b71eb972bad27b37af147cd
Felix Lai <felix.lai@hotmail.com>
2022-11-11 11:44:01 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,6 +19,17 @@ function App() {
     const forecastFetch = fetch(
       `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
     );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+    .then(async (response) => {
+      const weatherResponse = await response[0].json();
+      const forcastResponse = await response[1].json();
+
+      setCurrentWeather({ city: searchData.label, ...weatherResponse });
+      setForecast({ city: searchData.label, ...forcastResponse });
+    })
+    .catch(console.log);
+};
   return (
     <div className="App">
       <header className="App-header">
