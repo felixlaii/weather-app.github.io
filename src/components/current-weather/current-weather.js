589a499b1efdc93ed5bb87d461edc838cd2e62df
Felix Lai <felix.lai@hotmail.com>
2022-11-11 10:59:59 -0500

    Update current-weather.js

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 const CurrentWeather = ({ data }) => {
-    
+    return (
+        <div className="weather">
+            <div className="top">
+                <div>
+                    <p className="city">{data.city}</p>
+                    <p className="weather-description">{data.weather[0].description}</p>
+                </div>
+            </div>
+        </div>
+    )
 }
