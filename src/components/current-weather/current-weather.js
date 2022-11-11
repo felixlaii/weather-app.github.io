3ead5b6dcdce2a2fdf46e7a2678a427cab8fb9f4
Felix Lai <felix.lai@hotmail.com>
2022-11-11 11:13:11 -0500

    input incons

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -8,6 +8,9 @@ const CurrentWeather = ({ data }) => {
                     <p className="city">{data.city}</p>
                     <p className="weather-description">{data.weather[0].description}</p>
                 </div>
+                <img alt="weather"
+                className="weather-icon"
+                src={`icons/${data.weather[0].icon}.png`} />
             </div>
         </div>
     )
