5055b0666b1a484d756f853b14497b2d1d72d062
Felix Lai <felix.lai@hotmail.com>
2022-11-11 11:19:51 -0500

    Update forecast.js

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,2 +1,8 @@
 import React from "react";
-import { Accordion, AccordionItem, AccordionItemHeading, AccordionItemButton, AccordionItemPanel } from "react-accessible-accordion";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
