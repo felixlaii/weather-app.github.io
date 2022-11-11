f3a4423873820498d547d7336f0ba3f6fc589c7b
Felix Lai <felix.lai@hotmail.com>
2022-11-11 11:41:06 -0500

    Update search.js

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -24,6 +24,20 @@ const Search = ({ onSearchChange }) => {
                 }
             })
         }
+        const handleOnChange = (searchData) => {
+            setSearch(searchData);
+            onSearchChange(searchData);
+        };
+
+        return (
+            <AsyncPaginate
+            placeholder="Search for City"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
+            />
+        )
     })
   };
 };
